8460d000a9cd4493ad3e41e105e37e246562ae74
4 Rebase Academy <4rebaseacademy@iMac-de-4.local>
2023-12-05 07:01:49 +0100

    few linters errors fixed

--- assets/script/randomchg.js
+++ assets/script/randomchg.js
@@ -1,7 +1,7 @@
 function bckgrdColorChg () {
-// Adding colors 
+// Adding colors
   const colors = ['#ff0000', '#ff5723', '#32ff03', '#09fa02', '#98ff02']
-// Color display method
+  // Color display method
   const randomColor = colors[Math.floor(Math.random() * colors.length)]
   document.body.style.backgroundColor = randomColor
 }
